c541ed0c76b5d88a47af31e5ec5ef3669e7d4759
Furio Alessio <alefurio@hotmail.it>
2020-10-27 15:10:32 +0100

    added bullets that follow the current img

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ $(document).ready(function(){
                         //ARROW-NEXT//
     $('.arrowNext i').click(function(){ // intercetto il click di .arrowNext i
         var sliderImg = $('img.active'); // salvo nella var
+        var bullets = $('span.active');
 
         sliderImg.removeClass('active'); // al click, rimuovo la classe active
+        bullets.removeClass('active');
         if (sliderImg.next('img').length) {  //prima di aggiungere, verifico che dopo img corrente ci sia un' altra img
             sliderImg.next('img').addClass('active'); // se si, passo la classe active alla img successiva
+            bullets.next('').addClass('active');
         }
         else {
             $('.slider :first-child').addClass('active'); // se non c'e', assegno la classe active alla prima img in modo da poter ricominciare a sfogliare lo slider
